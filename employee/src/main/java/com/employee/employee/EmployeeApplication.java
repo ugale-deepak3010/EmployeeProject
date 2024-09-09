7aa267bd62a1413c9793cdf4523fa782bf155ab6
Dipak Ugale <ugale.deepak3010@gmail.com>
2024-09-09 09:55:14 +0000

    1

--- employee/src/main/java/com/employee/employee/EmployeeApplication.java
+++ employee/src/main/java/com/employee/employee/EmployeeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Started!");
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
